84f21fe9c5c87f6b68081cd5668211c4b5a75b4a
Loren Lima Martins <loren.lima@take.net>
2024-08-20 15:08:15 -0300

    feat: add services

---
+++ WeatherForecast/Services/WeatherForecastService.cs
@@ -0,0 +1,33 @@
+﻿using System.Text.Json;
+using WeatherForecast.Models;
+
+namespace WeatherForecast.Services
+{
+    public class WeatherForecastService
+    {
+        private readonly HttpClient _httpClient;
+        private readonly string _apiKey;
+
+        public WeatherForecastService(HttpClient httpClient, IConfiguration configuration)
+        {
+            _httpClient = httpClient;
+            _apiKey = configuration["WeatherApi:ApiKey"];
+        }
+
+        public async Task<WeatherForecastResponse> GetWeatherAsync(string cityName)
+        {
+            var encodedCity = System.Net.WebUtility.UrlEncode(cityName);
+            var requestUri = $"weather?key={_apiKey}&city_name={encodedCity}";
+
+            var response = await _httpClient.GetAsync(requestUri);
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                var weatherResponse = JsonSerializer.Deserialize<WeatherForecastResponse>(content, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+                return weatherResponse;
+            }
+
+            return null;
+        }
+    }
+}
